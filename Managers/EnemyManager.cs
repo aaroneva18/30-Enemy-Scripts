e1b62ca329c58faf55fe64583d4f0a963342bea0
aaroneva <aaronalvaradocalderon@gmail.com>
2025-02-16 23:56:41 -0600

    adding EnemyManager script and EnemyNavMesh script

---
+++ Managers/EnemyManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : CharacterManager
+{
+    public override void Dead() {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Respawn() {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetDefaultState() {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Spawn() {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
