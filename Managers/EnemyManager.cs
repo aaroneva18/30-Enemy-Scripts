241871bebdbac72fdc2320d2fcfe25f50034d1bd
aaroneva <aaronalvaradocalderon@gmail.com>
2025-02-17 23:52:30 -0600

    improving all enemyManager script

--- Managers/EnemyManager.cs
+++ Managers/EnemyManager.cs
@@ -1,9 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class EnemyManager : CharacterManager
 {
+    [SerializeField] private bool isEnemyCloseToPlayer;
+    [SerializeField] private bool isChasingPlayer;  
+    [SerializeField] private float radiusDetection;
+    [SerializeField] private float distanceToPlayer;
+    [SerializeField] private GameObject player;
+    [SerializeField] private LayerMask playerLayerMask;
+    [SerializeField] private Transform initialEnemyPosition;
+
+    private EnemyMovement enemyMovement;
+
+    private void Awake() {
+        SetDefaultState();
+    }
+
+    void Start()
+    {
+        KnowWherePlayerIs();
+    }
+
+    void Update()
+    {
+        DetectPlayerByCollisionRadious();
+        DistanceToPlayer();
+    }
+
+    void FixedUpdate() {
+        //implement states
+        if (isEnemyCloseToPlayer) {
+            if (isChasingPlayer) { 
+                enemyMovement.Move();
+                return; 
+            }
+            enemyMovement.SetDestination(player.transform);
+            isChasingPlayer = true;
+        }
+
+        if (DistanceToPlayer() > radiusDetection) { 
+            enemyMovement.TeleportTo(initialEnemyPosition);
+            enemyMovement.SetDestination(null);
+            isChasingPlayer = false;
+        }
+    }
+
+    public bool GetIsEnemyCloseToPlayer { get { return isEnemyCloseToPlayer; } }
+
     public override void Dead() {
         throw new System.NotImplementedException();
     }
@@ -12,23 +58,33 @@ public class EnemyManager : CharacterManager
         throw new System.NotImplementedException();
     }
 
-    public override void SetDefaultState() {
-        throw new System.NotImplementedException();
-    }
 
     public override void Spawn() {
         throw new System.NotImplementedException();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void DetectPlayerByCollisionRadious() {
+        isEnemyCloseToPlayer = Physics.CheckSphere(transform.position, radiusDetection, playerLayerMask);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void KnowWherePlayerIs() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Debug.DrawRay(transform.position, player.transform.position, Color.red);
+    }
+
+    public float DistanceToPlayer() {
+        return distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+    }
+
+    public override void SetDefaultState() {
+        try {
+            enemyMovement = GetComponent<EnemyMovement>();
+        } catch (System.Exception e) {
+            Debug.LogError(e);
+        }
+    }
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, radiusDetection);
     }
 }
