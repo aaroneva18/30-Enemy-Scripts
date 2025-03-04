63525e53d92249669e4777ec9fe169e95177db0c
aaroneva <aaronalvaradocalderon@gmail.com>
2025-03-03 23:03:48 -0600

    adding porpuses, rules and destinations

--- Movement/EnemyMovement.cs
+++ Movement/EnemyMovement.cs
@@ -14,11 +14,15 @@ public enum EnemyPorpuses {
 public class EnemyMovement : Movement {
 
     [SerializeField] private float distanceToPlayer = 0;
-    [SerializeField] private Transform destination;
+    [SerializeField] private Transform currentEnemyDestination;
     [SerializeField] private EnemyPorpuses currentPorpuse;
-    
+    [SerializeField] private bool enemyHasDestination = false;
+    [SerializeField] List<Transform> PatrolPosition;
+    [SerializeField] List<Transform> HidePosition;
+
 
     private Dictionary<Func<bool>, EnemyPorpuses> PorpusesRules;
+    private Dictionary<EnemyPorpuses, Transform> PosibleDestinations;
     private NavMeshAgent navMeshAgent;
     private EnemyManager enemyManager;
 
@@ -29,14 +33,18 @@ public class EnemyMovement : Movement {
     void Start()
     {
         SetEnemyPorpusesRules();
+        SetPosibleDesinations();
     }
 
     void Update()
     {
         SetEnemyPorpuse();
+        SetDestination();
+        Debug.Log(currentPorpuse);
     }
 
-    public Transform GetDestination() { return destination; }
+    public Transform GetDestination() { return currentEnemyDestination; }
+    public bool EnemyHasDestination { get { return enemyHasDestination; } }
 
     public override float CalculateCurrentSpeed() {
         throw new System.NotImplementedException();
@@ -51,42 +59,69 @@ public class EnemyMovement : Movement {
     }
 
     public override void Move() {
-        if (destination == null) { 
+        if (currentEnemyDestination == null) {
+            enemyHasDestination = false;
             navMeshAgent.isStopped = true;
-            navMeshAgent.SetDestination(SearchForPlayerPosition().position);
+            Debug.Log("Destination is null");
             return; 
         }
         navMeshAgent.isStopped = false;
-        navMeshAgent.SetDestination(destination.position);
+        navMeshAgent.SetDestination(currentEnemyDestination.position);
     }
-    public void SetDesination() {
+    private void SetPosibleDesinations() {
 
-        //hacer un diccionario de propositos y de destinos.
-        
+       PosibleDestinations = new Dictionary<EnemyPorpuses, Transform>()
+        {
+            {EnemyPorpuses.Patrol, FindCurrentPatrolPosition() },
+            {EnemyPorpuses.Chase, FindPlayerPosition() },
+            {EnemyPorpuses.Hide, FindCurrentHidePosition() }
+        };
+
     }
 
-    private Transform SearchForPlayerPosition() {
-        destination = GameObject.FindGameObjectWithTag("Player").transform;
-        string destinationisSet = destination ? "Destination is set" : "Destination is not set";
+    private Transform FindCurrentPatrolPosition() {
+        foreach (var temp_position in PatrolPosition) {
+            set
+        }
+        return null;
+    }
+
+    private Transform FindCurrentHidePosition() {
+        return null;
+    }
+
+    private Transform FindPlayerPosition() {
+        currentEnemyDestination = GameObject.FindGameObjectWithTag("Player").transform;
+        string destinationisSet = currentEnemyDestination ? "Destination is set" : "Destination is not set";
         Debug.Log(destinationisSet);
-        return destination;
+        return currentEnemyDestination;
 
     }
 
     private void SetEnemyPorpusesRules() {
         PorpusesRules = new Dictionary<Func<bool>, EnemyPorpuses>() 
         {
-            {() => enemyManager.GetIsEnemyCloseToPlayer, EnemyPorpuses.Chase },
-            {() => distanceToPlayer > 10, EnemyPorpuses.Hide },
-            {() => true, EnemyPorpuses.Patrol} //default state
+            {() => !enemyManager.IsEnemyCloseToPlayer && enemyManager.IsCharacterHealthy(), EnemyPorpuses.Patrol},
+            {() => enemyManager.IsEnemyCloseToPlayer, EnemyPorpuses.Chase },
+            {() => enemyManager.IsCharacterHurt() && distanceToPlayer > 50, EnemyPorpuses.Hide },
 
         };
     }
 
     private void SetEnemyPorpuse() {
-        foreach (var rule in PorpusesRules) {
-            if (rule.Key()) {
-                currentPorpuse = rule.Value;
+        foreach (var temp_rule in PorpusesRules) {
+            if (temp_rule.Key()) {
+                currentPorpuse = temp_rule.Value;
+                return;
+            }
+        }
+    }
+
+    private void SetDestination() {
+        foreach (var temp_porpuse in PosibleDestinations) {
+            if (temp_porpuse.Key == currentPorpuse) {
+                currentEnemyDestination = temp_porpuse.Value;
+                enemyHasDestination = true; 
                 return;
             }
         }
